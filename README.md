e43b9f2f7458283fa9684e8fbeaf626e19a153aa
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-03-04 08:45:00 +0600

    Vue Starter kits

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue_Starter_Kits
---
+++ resources/js/pages/Demo.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Demo form inertia js</h1>
+  <p>This is a Blade template: {{ person.name }}</p>
+</template>
+
+<style scoped>
+</style>
